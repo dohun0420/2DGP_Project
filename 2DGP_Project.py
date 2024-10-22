cb969cb3f14147bfa01c4fb626090396dec680f0
dohunkim(main) <dohun5841@gmail.com>
2024-10-22 22:04:20 +0900

    sad happy 클래스 생성
    시험기간으로 인해 부득이하게 간단한 작업 수행

--- 2DGP_Project.py
+++ 2DGP_Project.py
@@ -8,6 +8,31 @@ from Store import Store
 from UI import UI
 import time
 
+class Happy:
+    def __init__(self, x, y):
+        self.image = load_image('Happy.png')
+        self.x, self.y = x, y
+        self.frame = 0
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 500 , 0, 500, 500, self.x, self.y, 25, 25)
+
+    def update(self):
+        pass
+
+class Sad:
+    def __init__(self, x, y):
+        self.image = load_image('Sad.png')
+        self.x, self.y = x, y
+        self.frame = 0
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 225 , 0, 225, 225, self.x, self.y, 25, 25)
+
+    def update(self):
+        pass
+
+
 def handle_events():
     global running
     global player
