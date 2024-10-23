7d4d34175158f0d56a1d196778bafe36fe626afb
dohunkim(main) <dohun5841@gmail.com>
2024-10-23 22:01:05 +0900

    금괴 채굴에 성공시 웃는 이모지 출력 실패시 우는 이모지 출력 구현

--- 2DGP_Project.py
+++ 2DGP_Project.py
@@ -1,12 +1,12 @@
 from pico2d import *
-
+import random
+import time
 from Dig_ani import Dig_ani
 from Gold import Gold
 from Map import Map
 from Player import Player
 from Store import Store
 from UI import UI
-import time
 
 class Happy:
     def __init__(self, x, y):
@@ -31,7 +31,6 @@ class Sad:
 
     def update(self):
         pass
-
 
 def handle_events():
     global running
@@ -72,11 +71,17 @@ def reset_world():
     global space_mode
     global space_pressed_time
     global dig_ani
+    global happy
+    global sad
+    global result_mode
+    global result_time
 
     running = True
     store_mode = False
     space_mode = False
     space_pressed_time = 0
+    result_mode = False
+    result_time = 0
     world = []
 
     map = Map()
@@ -94,14 +99,28 @@ def reset_world():
     store = Store()
 
     dig_ani = None
+    happy = None
+    sad = None
 
 def update_world():
     global space_mode
     global space_pressed_time
+    global result_mode
+    global result_time
+    global happy, sad
 
     if space_mode:
         if time.time() - space_pressed_time >= 5:
-            gold.increase()
+            # 70% 확률로 금 증가
+            if random.random() < 0.7:
+                gold.increase()
+                happy = Happy(player.x, player.y + 40)
+                result_time = time.time()
+                result_mode = 'happy'
+            else:
+                sad = Sad(player.x, player.y + 40)
+                result_time = time.time()
+                result_mode = 'sad'
             space_mode = False
         else:
             if dig_ani:
@@ -110,6 +129,12 @@ def update_world():
     if not space_mode:
         for o in world:
             o.update()
+
+    if result_mode:
+        if time.time() - result_time >= 0.5:
+            result_mode = False
+            happy = None
+            sad = None
 
 def render_world():
     clear_canvas()
@@ -120,6 +145,11 @@ def render_world():
             o.draw()
         if space_mode and dig_ani:
             dig_ani.draw()
+        if result_mode:
+            if result_mode == 'happy' and happy:
+                happy.draw()
+            elif result_mode == 'sad' and sad:
+                sad.draw()
     update_canvas()
 
 open_canvas(1000, 600)
