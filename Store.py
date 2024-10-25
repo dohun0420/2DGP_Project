98a7ec481992aef4c3b5021e43d869327524b897
dohunkim(main) <dohun5841@gmail.com>
2024-10-25 22:59:16 +0900

    상점 이미지 구현

--- Store.py
+++ Store.py
@@ -1,9 +1,8 @@
 from pico2d import load_image
 
-
 class Store:
     def __init__(self):
-        self.image = load_image('store_test.png')
+        self.image = load_image('Store.png')
 
     def draw(self):
         self.image.draw(500, 300, 1000, 600)
